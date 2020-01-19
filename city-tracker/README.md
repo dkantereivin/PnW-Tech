7466c8984803e2a43ba5d4f9209612bc1d73e53b
D. Kanter Eivin <kanter.dev@gmail.com>
2020-01-19 14:20:56 -0500

    Documentation, readme, moved npm project.
    - moved package.json and package-lock.json inside the appropriate city-tracker folder
    - added README.md
    - documented code more thoroughly with comments
    - added indication that current version is untested

---
+++ city-tracker/README.md
@@ -0,0 +1,44 @@
+# City Tracker
+Author: [Canter#0548](https://github.com/dkantereivin)
+
+## Purpose
+To monitor a list of subscribed alliances, downloading the alliances cities and storing them historically (each day) in the database. This is useful for intelligence purposes.
+
+## API
+This program's output is a web API, and it periodically fetches and stores data in the database.
+
+## Endpoints:
+This API currently has a single endpoint:
+#### GET /cities/:date&:allianceID
+Both parameters are required. To fetch the current date, write it or write `current` in the date slot. Dates should use the `YYYYMMDD` format.
+
+### Authentication:
+This API uses header based authentication. In **all** calls to the database, include in the header:
+```json
+{
+    "user": "your username",
+    "pswd": "your pswd"
+}
+```
+Unauthenticated calls to *any* endpoint will result in a `401: Unauthorized` HTTP response.
+
+## Installation
+__Requirements:__
+- nodejs + npm
+- expressjs
+- dotenv (and a .env file, see below for template)
+- pnw4js
+- mongodb (npm) and a running mongodb server
+
+Installation:
+```shell
+git clone https://github.com/dkantereivin/PnW-War-Tech
+cd city-tracker
+npm i
+```
+Create a `.env` file in the folder, with the below template.
+
+To execute, run `node server.js`.
+
+## LICENSE
+This work is the sole intellectual property of it's original creator. Do not reproduce, modify, deploy, or use in any other way this code without express permission from the original creator.
--- city-tracker/server.js
+++ city-tracker/server.js
@@ -1,8 +1,15 @@
+// UNTESTED
 require('dotenv').config();
 const moment = require('moment');
 const express = require('express');
 const PnW = require('pnw4js');
 const MongoClient = require('mongodb').MongoClient;
+
+/**
+ * SUBSCRIBED: an array of alliance IDs to subscribe to. Caution regarding token usage.
+ * As a suggestion, prioritize more important alliances earlier on so that if API calls run out, key calls will be stored.
+ */
+const SUBSCRIBED = [];
 
 // init
 const app = express();
@@ -18,8 +25,17 @@ const authSchema = db.collection('auth');
 
 // periodic
 const REFRESH_TIME = 86760000;
-setInterval(saveCitySnapshot, REFRESH_TIME);
+setInterval(() => {
+    for (let i = 0; i < SUBSCRIBED; i++)
+        saveCitySnapshot(SUBSCRIBED[i]);
+}, REFRESH_TIME);
 
+/**
+ * Performs a GET request to the PNW for the specified alliance, and then iterates it's cities.
+ * All data from each city is stored in the MongoDB database as an array.
+ * Uses aa.city_count + 1 calls per execution.
+ * @param {String} aa The alliance to snapshot.
+ */
 async function saveCitySnapshot(aa/*id*/)
 {
   const alliance = await pnw.alliance(aa); // a nice phat library i made myself :D
@@ -28,7 +44,6 @@ async function saveCitySnapshot(aa/*id*/)
     cities[i] = pnw.city(cities[i]);
   await Promise.all(cities);
 
-  
   citiesSchema.insertOne({
     date: moment().startOf('D').toISOString(),
     alliance: aa,
@@ -36,6 +51,13 @@ async function saveCitySnapshot(aa/*id*/)
   });
 }
 
+/**
+ * A middleware authentication function called on every call to the server.
+ * Checks for the existence of the username and password specified in headers against the database.
+ * @param {Express.Request} req Modified to include the calling user records from the database.
+ * @param {Express.Response} res
+ * @param {Express.NextFunction} next 
+ */
 async function authenticateRequest(req, res, next)
 {
 
@@ -49,6 +71,11 @@ async function authenticateRequest(req, res, next)
     res.sendStatus(401);
 }
 
+/**
+ * Responds with the full historical record of cities for a specified date.
+ * @param {String} date The date to search for, using format of 'current' day OR 'YYYYMMDD'.
+ * @param {String} allianceID The alliance ID, NumberResolvable.
+ */
 server.get('/cities/:date&:allianceID', (req, res) => {
     let {date, allianceID} = req.params;
     if (date === 'current' || typeof date !== 'string')
